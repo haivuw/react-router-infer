449bf329ceeb00fcd8d7b48e1756b007a34f7dae
haivuw <haivuw@gmail.com>
2024-01-22 14:35:34 +0700

    v

--- create-release.sh
+++ create-release.sh
@@ -2,15 +2,15 @@
 
 repo="haivuw/react-router-infer"
 
-current_version=$(gh release list --repo "$repo" --limit 1 | cut -f1) || 0
+current_version=$(gh release list --repo "$repo" --limit 1 | cut -f1) 
 
 version=$(jq -r '.version' package.json)
 
-read -p "Confirm updating from $current_version to $version? (y/n):" answer
+read -p "Confirm updating from v$current_version to v$version? (y/n):" answer
 
 if [ "$answer" == "y" ]; then
   echo "Updating..."
-  gh release create "$version" --repo="$repo" --title="$version" --generate-notes
+  gh release create v"$version" --repo="$repo" --title=v"$version" --generate-notes
   echo "Done."
 else
   echo "Update canceled."
